95a66e91bf2f8e75b23cf27b20169fa5841faae6
alanpen.tw@gmail.com <alanpen.tw@gmail.com>
2022-09-16 17:15:00 +0800

    modify ta_example file

--- ta_example.py
+++ ta_example.py
@@ -20,4 +20,26 @@ data = ta.add_all_ta_features(df,"Open", "High","Low","Close","Volume",fillna=Tr
 ma = ta.trend.SMAIndicator(df['Close'],10,fillna=True)
 
 ma = ma.sma_indicator()
-print(ma)
+#print(ma)
+
+
+#呼叫布林通道
+indicator_bb = ta.volatility.BollingerBands(close=df['Close'],window=20,window_dev=2)
+#布林中線
+bb_bbm = indicator_bb.bollinger_mavg()
+#布林上線
+bb_bbh = indicator_bb.bollinger_hband()
+#布林下線
+bb_bbl = indicator_bb.bollinger_lband()
+
+print('布林中線\n',bb_bbm)
+
+
+#返回close是否大於布林上軌, true = 1 , false = 0
+bb_bbhi = indicator_bb.bollinger_hband_indicator()
+#返回close是否大於布林下軌, true = 1 , false = 0
+bb_bbli = indicator_bb.bollinger_lband_indicator()
+#布林帶寬
+bb_bbw = indicator_bb.bollinger_wband()
+#布林%b指標(%b值 = (收盤價 布林帶下軌值) / ( 布林帶上軌值 布林帶下軌值))
+bb_bbp = indicator_bb.bollinger_pband()
