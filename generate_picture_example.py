d29de6baf2f8df24712df281c0a547c56afd6c22
alanpen.tw@gmail.com <alanpen.tw@gmail.com>
2022-09-26 17:29:59 +0800

    add pythone

--- generate_picture_example.py
+++ generate_picture_example.py
@@ -4,6 +4,19 @@ import pandas as pd
 import mpl_finance as mpf
 import matplotlib.pyplot as plt
 
+
+'''
+畫K線圖及成交量圖的邏輯
+
+1.定義畫布(plt.figure)
+2.定義模板(plt.GridSpec(3,20)) - 如有需多張大小不一的圖片,無需要可省略
+3.依照模板定義具備不同大小的區塊(fig.add_subplot(grid[0:2,1:]))
+  or 定義大小均等的區塊(fig.add_subplot(grid[2,1,1]))
+4.在定義出的區塊自由揮灑
+'''
+
+
+
 #yfinance產出台積電股價資料
 stock = yf.Ticker('2330.TW')
 #get 20200101-20220922
@@ -47,13 +60,47 @@ z:指定這張圖應該放的位置
 '''
 
 #add_subplot設置為１，並將ax指定為此區塊
-ax = fig.add_subplot(1,1,1)
+#ax = fig.add_subplot(1,1,1)
+
+#定義模板大小: 3*20
+grid = plt.GridSpec(3,20)
+
+#區塊1畫主圖,給主圖2格block,並在左方保留一格block
+ax = fig.add_subplot(grid[0:2,1:])
+'''
+------------------------
+|    | z   | z   | z   |
+------------------------
+|    | z   | z   | z   |
+------------------------
+|    |     |     |     |
+------------------------
+'''
+
+
+#區塊2畫子圖,給子圖1格block,並在左方保留一格block
+ax2 = fig.add_subplot(grid[2:,1:])
+'''
+------------------------
+|    |     |     |     |
+------------------------
+|    |     |     |     |
+------------------------
+|    | z   | z   | z   |
+------------------------
+'''
 
 
 #畫分K , 依序填入開、收、高、低、寬、紅綠K以及透明度
 mpf.candlestick2_ochl(ax, df['Open'], df['Close'], df['High'],
     df['Low'], width=0.6, colorup='r', colordown='g',alpha=0.75)
 
+
+#使用 mpl_volume_overlay 畫出量,並將之畫在區塊 ax2 
+mpf.volume_overlay(ax2, df['Open'], df['Close'], df['Volume'], colorup='r', colordown='g')
+
+#先用DateFrame 將 'Date'的值取出後,再使用lambda做日期的轉換
+convert_date = pd.DataFrame(df.index[::30])['Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
 
 
 '''
@@ -77,9 +124,6 @@ ax.set_xticks(range(0,len(df.index),30))
 #因 dataframe 不允許直接對 index 做任何相關運算,故下面的程式會掛掉
 #ax.set_xticklabels(df.index[::30].apply(lambda x: x.strftime('%Y-%m-%d')),rotation=90)
 
-#先用DateFrame 將 'Date'的值取出後,再使用lambda做日期的轉換
-convert_date = pd.DataFrame(df.index[::30])['Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
-
 ax.set_xticklabels(convert_date,rotation=90,fontsize=6)
 
 
@@ -88,19 +132,32 @@ ax.set_xticklabels(convert_date,rotation=90,fontsize=6)
 plot() : 畫線
 .values : 將 dataframe 轉為 numpy.array 的格式
 '''
-ax.plot(df['bbm'].values,color='b',label='bbm')
-ax.plot(df['bbh'].values,color='g',label='bbh')
-ax.plot(df['bbl'].values,color='r',label='bbl')
+ax.plot(df['bbm'].values,color='b',label='bbm', linestyle="--")
+ax.plot(df['bbh'].values,color='g',label='bbh', linestyle="--")
+ax.plot(df['bbl'].values,color='r',label='bbl', linestyle="--")
+
+
+#設置區塊2的刻度
+ax2.set_xticks(range(0,len(df.index),30))
+#設置區塊2的刻度的值
+ax2.set_xticklabels(convert_date,rotation=90,fontsize=6)
 
 
 #設置圖片標題
-plt.title(f'2330 Stock Price')
+ax.set_title(f'2330 Stock Price')
 #設置X軸名稱
-plt.xlabel('Date')
+ax.set_xlabel('Date')
 #設置Y軸名稱
-plt.ylabel('Price')
-#設置legend才會有label跑出來
-plt.legend(loc='best')
+ax.set_ylabel('Price')
 
+#防止圖片重疊
+fig.tight_layout()
+
+#設置legend才會有label跑出來
+#ax.legend(loc='best')
+ax.legend()
+
+#儲存圖檔為png
+plt.savefig('test.png')
 
 plt.show()
